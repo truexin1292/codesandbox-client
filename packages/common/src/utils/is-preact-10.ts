f03ac9ca5ceb59a4f4c059e9665cf232c7ef633f
Sara Vieira <hey@iamsaravieira.com>
2020-10-03 19:39:20 +0200

    fix preact issue

--- packages/common/src/utils/is-preact-10.ts
+++ packages/common/src/utils/is-preact-10.ts
@@ -5,12 +5,14 @@ export function isPreact10(
   dependencies: Dependencies,
   devDependencies: Dependencies
 ) {
-  const preactVersion = dependencies.preact || devDependencies.preact;
-  if (preactVersion) {
-    return (
-      /^[a-z]/.test(preactVersion) ||
-      semver.intersects(preactVersion, '>=10.0.0')
-    );
+  if (dependencies) {
+    const preactVersion = dependencies.preact || devDependencies.preact;
+    if (preactVersion) {
+      return (
+        /^[a-z]/.test(preactVersion) ||
+        semver.intersects(preactVersion, '>=10.0.0')
+      );
+    }
   }
 
   return false;
